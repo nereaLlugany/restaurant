f09161310b11858428890f80fe5a48c69533398b
nereaLlugany <nerea.llugany.montoya@gmail.com>
2024-12-05 12:05:25 +0100

    Start Creating Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x-guest-layout>
+    <div class = "py-5">
+        <div class = "container max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h2 class = "front-semibold txt-xl text-gray-800 leading-tight">
+                {{__("Home page")}}
+            </h2>
+            <section>
+                <p>
+                    sadsfsdfsfsdfdssdfsdf
+                </p>
+            </section>
+        </div>
+    </div>
+    <x-footer />
+</x-guest-layout>
